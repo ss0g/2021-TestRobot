d9d46093d451892b99d84dcde434e58e91d1b1f6
ss0g <55930267+ss0g@users.noreply.github.com>
2022-01-16 13:23:52 -0800

    initial commit for 2022 import version

---
+++ src/main/java/com/spartronics4915/frc/RobotContainer.java
@@ -0,0 +1,75 @@
+package com.spartronics4915.frc;
+
+import com.spartronics4915.frc.Constants;
+import com.spartronics4915.frc.commands.ExampleCommand;
+import com.spartronics4915.frc.commands.JoystickMotorCommand;
+import com.spartronics4915.frc.subsystems.ExampleSubsystem;
+import com.spartronics4915.frc.subsystems.JoystickMotor;
+import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import edu.wpi.first.wpilibj2.command.ScheduleCommand;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.RobotBase;
+
+//
+
+/**
+ * This class is where the bulk of the robot should be declared.
+ * Since command-based is a "declarative" paradigm, very little robot logic
+ * should actually be handled in the {@link Robot} periodic methods
+ * (other than the scheduler calls). Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer
+{
+    // The robot's subsystems and commands are defined here...
+    // public final ExampleSubsystem mExampleSubsystem;
+    public final JoystickMotor mJoystickMotor;
+
+    // public final ExampleCommand mAutoCommand;
+    public final JoystickMotorCommand mJoystickMotorCommands;
+
+    // private final int kJoystickPort = Constants.OI.kJoystickId;
+    public static final Joystick mDriverController = new Joystick(Constants.OI.kJoystickId);
+
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer()
+    {
+
+
+        // ...and constructed here.
+        // mExampleSubsystem = ExampleSubsystem.getInstance();
+        // mAutoCommand = new ExampleCommand(mExampleSubsystem);
+
+        mJoystickMotor = JoystickMotor.getInstance();
+        mJoystickMotorCommands = new JoystickMotorCommand(mJoystickMotor);
+
+        configureButtonBindings();
+        SmartDashboard.putString("Container","Completed");
+
+        mJoystickMotor.setDefaultCommand(new JoystickMotorCommand(mJoystickMotor));
+    }
+
+    /** Use this method to define your button ==> command mappings. */
+    private void configureButtonBindings()
+    {
+
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand()
+    {
+        return null;
+    }
+}
---
+++ src/main/java/com/spartronics4915/frc/subsystems/ExampleSubsystem.java
@@ -0,0 +1,73 @@
+package com.spartronics4915.frc.subsystems;
+
+import com.spartronics4915.frc.Constants;
+import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+// import com.revrobotics.CANError;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+/**
+ * We're not using Talon any more
+ * import edu.wpi.first.wpilibj.motorcontrol.Talon;
+ * /
+
+/**
+ * Detailed description of ExampleSubsystem.
+ */
+public class ExampleSubsystem extends SpartronicsSubsystem
+{
+    private static ExampleSubsystem sInstance = null;
+    // The subsystem's hardware is defined here...
+    private static CANSparkMax mTestMotor;
+
+    /** Creates a new ExampleSubsystem. */
+    public ExampleSubsystem()
+    {
+        boolean success = true;
+        try
+        {
+            mTestMotor = new CANSparkMax(Constants.kTestMotorId,MotorType.kBrushless);
+        }
+        catch (Exception exception)
+        {
+            logException("Could not construct hardware: ", exception);
+            success = false;
+        }
+        logInitialized(success);
+    }
+
+    public static ExampleSubsystem getInstance()
+    {
+        if (sInstance == null) {
+            sInstance = new ExampleSubsystem();
+        }
+        return sInstance;
+    }
+
+    // Subsystem methods - actions the robot can take - should be placed here.
+
+    public void startTestMotor(double speed) {
+        SmartDashboard.putString("Motor","Starting motor");
+        mTestMotor.set(speed);
+        SmartDashboard.putNumber("Speed",mTestMotor.get());
+    }
+
+    public void stopTestMotor() {
+        SmartDashboard.putString("Motor","Stopping motor");
+        mTestMotor.set(0.0);
+    }
+
+    /** This method will be called once per scheduler run. */
+    @Override
+    public void periodic() {
+        SmartDashboard.putString("Periodic","Starting");
+    }
+
+    /** This method will be called once per scheduler run during simulation. */
+    @Override
+    public void simulationPeriodic() {}
+}
